1144c9265dc931fc56ea6530887f6880c7e95849
Raya Diez de Urdanivia <rayadiez@student.uiwtx.edu>
2025-10-29 16:38:46 -0500

    Updated Files and Engine

--- Assets/Scripts/History.cs
+++ Assets/Scripts/History.cs
@@ -5,16 +5,20 @@ using UnityEngine.UI;
 
 public class History : MonoBehaviour
 {
- public Text lastname;
- public Text lastplace;
- public Text lastdistortion;
+    public Text lastname;
+    public Text lastplace;
+    public Text lastdistortion;
  
  
- void Start(){
-     
-     lastname.text = FindObjectOfType<Navigator>().name;
-     lastplace.text = FindObjectOfType<Navigator>().place;
-     lastdistortion.text = FindObjectOfType<Navigator>().distortion;
+    void Start()
+    {
+        PlayerPrefs.SetString("LastName", FindFirstObjectByType<SNavigator>().name);
+        lastname.text = PlayerPrefs.GetString("LastName");
+        PlayerPrefs.SetString("LastPlace", FindFirstObjectByType<SNavigator>().place);
+        lastplace.text = PlayerPrefs.GetString("LastPlace");
+        PlayerPrefs.SetString("LastDistorsion", FindFirstObjectByType<SNavigator>().distortion);
+        lastdistortion.text = PlayerPrefs.GetString("LastDistorsion");
+
      
      }
 }
--- README.md
+++ README.md
@@ -7,6 +7,7 @@ __A Fanmade Recreation of the Metaverse Navigator App seen in [Persona 5](https:
 |Engine|Version|
 |------|-------|
 |Unity|6000.0.25f1|
+|Unity|6000.2.8f1|
 
 ## Function:
 This proyect has these basic main functions:
